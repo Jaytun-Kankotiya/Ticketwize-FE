f7181e05f18b95430ce277fa71df3f76758561e3
ravin-rakholiya <ravinkumarrakh@gmail.com>
2023-07-08 22:03:28 -0400

    made required changes for payment

--- src/config/env.js
+++ src/config/env.js
@@ -76,7 +76,7 @@ function getClientEnvironment(publicUrl) {
         // This should only be used as an escape hatch. Normally you would put
         // images into the `src` and `import` them in code to get their paths.
         PUBLIC_URL: publicUrl,
-        API_URL: "http://localhost:8000"
+        API_URL: "http://15.156.172.206:8000/"
       }
     );
   // Stringify all values so we can feed into Webpack DefinePlugin
